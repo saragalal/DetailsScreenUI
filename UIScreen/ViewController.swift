ef426db27c5d7f0e38c796428acdc5507e9569df
sara <sara.galal@ibtikar.net.sa>
2019-09-30 12:50:01 +0200

    Initial Commit

---
+++ UIScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIScreen
+//
+//  Created by sara.galal on 9/30/19.
+//  Copyright © 2019 sara.galal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
